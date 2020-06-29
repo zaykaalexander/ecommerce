4739d22305aeaf8d2323e55f2a3fad99e3062f71
Alexander Zayka <zand.johnny@gmail.com>
2020-06-29 23:11:04 +0700

    Code style

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
 };
